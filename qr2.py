0ad3027cc7ff404ae24d520ed1c50e8c8d63eee5
Annu9111 <aksoni911190@gmail.com>
2026-01-22 15:27:40 +0530

    generating qr code

--- qr2.py
+++ qr2.py
@@ -3,3 +3,5 @@ from PIL import Image
 qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4)
 qr.add_data("https://www.youtube.com/channel/UC4xFnWBlNM1G0vEGVc7Ermg")
 qr.make(fit=True)
+img=qr.make_image(fill_color="green",back_color="pink")
+img.save("qr2.png")
